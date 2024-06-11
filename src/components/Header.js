ffa83493fe3d9a8de30f70f6c5032f3be36296a9
Priya Gupta <priyagupta052k@gmail.com>
2024-06-11 23:53:02 +0530

    Menu Bar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,41 @@
 import React from "react";
-import {ReactDOM} from "react-dom/client";
-import Button from 'react-bootstrap/Button';
+import { ReactDOM } from "react-dom/client";
+import { Col, Container, Row, Button, Form } from "react-bootstrap";
+import InputGroup from "react-bootstrap/InputGroup";
 
-function Header (){
-    return(
-        <header>
-        <>
-        <h1>Logo</h1>
-        </>
-        </header>
-    )
+function Header() {
+  return (
+    <header>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Logo</h1>
+          </Col>
+          <Col>
+            <Button variant="outline-info">Info</Button>
+          </Col>
+          <Col>
+            <InputGroup size="sm" className="mb-3">
+              <InputGroup.Text id="inputGroup-sizing-sm">Small</InputGroup.Text>
+              <Form.Control
+                aria-label="Small"
+                aria-describedby="inputGroup-sizing-sm"
+              />
+            </InputGroup>
+          </Col>
+          <Col>
+            <Button variant="outline-info">EN</Button>
+          </Col>
+          <Col>
+          <Button variant="outline-info">sign-in</Button>
+          </Col>
+          <Col>
+            <Button>Cart</Button>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
