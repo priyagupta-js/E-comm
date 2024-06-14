0e23080771f42dcf416e5e7cb283335de9adfe48
Priya Gupta <priyagupta052k@gmail.com>
2024-06-15 00:57:33 +0530

    Menu Bar updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '@fortawesome/fontawesome-free/css/all.css';
 import {ReactDOM} from "react-dom/client";
 import Header from "../src/components/Header";
 import Home from "./components/Home";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {faCartShopping} from "react-icons/fa";
+
 function Header() {
   return (
     <header className="menu-bar">
@@ -12,9 +12,7 @@ function Header() {
         Ranchi, Jharkhand, India
       </div>
       <div className="search-bar">
-        <button type="submit">
-          <i class="fa fa-search"></i>
-        </button>{" "}
+        <i class="fa fa-search"></i>
         <input
           type="text"
           className="search"
@@ -22,36 +20,15 @@ function Header() {
           name="search"
         ></input>
       </div>
-      <div className="btn-login"><span className="login-text">Login</span></div>
-      <div className="btn-cart"><button className="cart"><FontAwesomeIcon icon={faCartShopping} />My Cart</button></div>
-      {/* <Container>
-        <Row> */}
-      {/* <Col className="logo">
-            <h1>Logo</h1>
-          </Col> */}
-      {/* <Col className="location-div">
-            <span className="location">
-              
-            </span>
-          </Col> */}
-      {/* <Col xs={4} className="search-bar">
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="basic-addon1" className="search">@</InputGroup.Text>
-              <Form.Control
-                placeholder="Search"
-                aria-label="Search"
-                aria-describedby="basic-addon1"
-              />
-            </InputGroup>
-          </Col> */}
-      {/* <Col xs={2}className="btn-main">
-          <span className="btn-login">Login</span>
-          </Col> */}
-      {/* <Col className="btnCart-main">
-            <Button className="btn-cart">Cart</Button>
-          </Col>
-        </Row>
-      </Container> */}
+      <div className="btn-login">
+        <span className="login-text">Login</span>
+      </div>
+      <div className="btn-cart">
+        <button className="cart">
+          <i class="fa-solid fa-cart-shopping"></i>&nbsp;
+          My Cart
+        </button>
+      </div>
     </header>
   );
 }
